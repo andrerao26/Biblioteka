659c9a695b0a3c9dcd6f1cc508ed54388ab14bf9
3f05 <3f05@Liceospalla.local>
2017-03-06 11:55:40 +0100

    first commit

---
+++ Biblioteka/Biblioteka/Libro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    class Libro
+    {
+        public string titolo { get; set; }
+        public string autore { get; set; }
+        public enum genere { };
+        public string isbn { get; set; }
+        private bool _prestato;
+        public bool prestato { get { return true; } }
+        public Utente possessore { get; set; }
+
+        public Libro()
+        {
+
+        }
+
+        public string describeLibro()
+        {
+            return "qua descrivo il libro";
+        }
+
+        public override string ToString()
+        {
+            return titolo + " " + isbn;
+        }
+
+        public bool prestaLibro()
+        {
+            return true;
+        }
+    }
+}
---
+++ Biblioteka/Biblioteka/Utente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    class Utente
+    {
+        public string nome { get; set; }
+        public string cognome { get; set; }
+        public string codiceFiscale { get; set; }
+        public DateTime dataNascita { get; set; }
+        public List<Libro> libriPrestito = new List<Libro>();
+
+        public Utente()
+        {
+
+        }
+
+        public string describeUtente()
+        {
+            return "qua descrivo l'utente";
+        }
+
+        public override string ToString()
+        {
+            return nome + " " + cognome;
+        }
+
+        public string describeLibriPrestito()
+        {
+            return "qua descrivo tutti i libri in prestito all'utente";
+        }
+    }
+}
